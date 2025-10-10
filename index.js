b67fd3357f5adf91d58bd5c65a13e2ae1d9837fc
REDROJO23 <wilfridolop26@gmail.com>
2025-10-09 19:35:09 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const cors = require('cors');
 require('dotenv').config();
 require('./db'); // Conexión a MongoDB
 
-const lockersRoutes = require('./routes/lockers'); // Asegúrate que la carpeta se llame "routes"
+const lockersRoutes = require('./routes/lockers');
 
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); // ✅ Esto permite leer req.body correctamente
+
 app.use('/', lockersRoutes);
 
 const PORT = process.env.PORT || 5000;
